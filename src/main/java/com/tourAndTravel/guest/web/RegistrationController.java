2e250e518639f4beeb2dcd8e2b6d1d80a5ff70d9
Sinkumen <Sinkumen@gmail.com>
2019-01-16 03:00:46 +0300

    Commit with security

--- src/main/java/com/tourAndTravel/guest/web/RegistrationController.java
+++ src/main/java/com/tourAndTravel/guest/web/RegistrationController.java
@@ -2,6 +2,8 @@ package com.tourAndTravel.guest.web;
 
 import javax.validation.Valid;
 
+import com.tourAndTravel.guest.model.Role;
+import com.tourAndTravel.guest.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -15,15 +17,20 @@ import org.springframework.web.servlet.ModelAndView;
 import com.tourAndTravel.guest.model.Users;
 import com.tourAndTravel.guest.repository.UserRepository;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 @Controller
 @RequestMapping("/registration")
 public class RegistrationController {
 
 	UserRepository userRepository;
+	RoleRepository roleRepository;
 	
 	@Autowired
-	  public RegistrationController(UserRepository userRepository) {
+	  public RegistrationController(UserRepository userRepository,RoleRepository roleRepository) {
 	    this.userRepository = userRepository;
+	    this.roleRepository = roleRepository;
 	   
 	  }
 
@@ -52,6 +59,8 @@ public class RegistrationController {
 	    user.setPassword(bt.encode(user.getPassword()));
 		System.out.println(user.getFirstName());
 		System.out.println(user.getLastName());
+		Role role = roleRepository.findByRole("TOURIST");
+		user.setRoles(new HashSet<Role>(Arrays.asList(role)));
 		userRepository.save(user);
 	    
 
